fe966b69a52b9548ef06bc013dbd78c850c81540
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-21 15:54:52 +0100

    Adsadsadasdasdasdasdasdasd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import './App.css'
-
+function showNotification(title:string, options:any) {
+  if ("Notification" in window && Notification.permission === "granted") {
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.showNotification(title, options);
+    });
+    console.log("kaka")
+  }
+}
 function App() {
+  if ("Notification" in window) {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("Notification permission granted");
+      } else {
+        console.warn("Notification permission denied");
+      }
+    });
+  }
+  showNotification("New Task Added", {
+    body: "Check out the new task from your teacher.",
+  });
   return (
     <>
     </>
