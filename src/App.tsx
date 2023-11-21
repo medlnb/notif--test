8015d710a9e0ac1c1fc6b174a8230a8dd2947a97
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-21 15:45:06 +0100

    asdasd

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import './App.css'
-import addNotification from 'react-push-notification'
+
 
 function App() {
-  const clicktonotify = () => {
-    addNotification({
-      title: "khra",
-      message: "chkopi ya chkopi",
-      duration: 4000,
-      native: true
-    })
-  }
+
 
   return (
     <>
-      <button onClick={() => clicktonotify()}>
-        click here
-      </button>
+
     </>
   )
 }
