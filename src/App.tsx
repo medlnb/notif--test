12b66ed5f6947296f80a87ccbc497a2a9954dc6a
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-23 23:54:45 +0100

    asdadsa

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import './App.css';
 
 
 function App() {
-
-  navigator.serviceWorker.register('sw.js');
-  Notification.requestPermission(function (result) {
-    if (result === 'granted') {
-      navigator.serviceWorker.ready.then(function (registration) {
-        registration.showNotification('Notification with ServiceWorker');
-      });
-    }
+  navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    registrations[0].showNotification("title");
   });
+  // navigator.serviceWorker.register('sw.js');
+  // Notification.requestPermission(function (result) {
+  //   if (result === 'granted') {
+  //     navigator.serviceWorker.ready.then(function (registration) {
+  //       registration.showNotification('Notification with ServiceWorker');
+  //     });
+  //   }
+  // });
   return (
     <>
       <button >
