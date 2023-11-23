1e93bd07710e52e36d08f6731edd5ccd4f1748dc
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-24 00:00:00 +0100

    asdasdsa

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import './App.css';
 
 
 function App() {
-  navigator.serviceWorker.getRegistrations().then(function (registrations) {
-    registrations[0].showNotification("title");
+  navigator.serviceWorker.register('sw.js');
+
+  Notification.requestPermission().then(function (permission) {
+    if (permission != "granted") {
+      alert("Notification failed!");
+      return;
+    }
+
+    navigator.serviceWorker.ready.then(function (registration) {
+      registration.showNotification("Hello world", { body: "Here is the body!" });
+    });
+
   });
-  // navigator.serviceWorker.register('sw.js');
-  // Notification.requestPermission(function (result) {
-  //   if (result === 'granted') {
-  //     navigator.serviceWorker.ready.then(function (registration) {
-  //       registration.showNotification('Notification with ServiceWorker');
-  //     });
-  //   }
-  // });
+
   return (
     <>
       <button >
