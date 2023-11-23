595980d30d805bbcabb275832d7a026b1c1a19ca
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-23 23:50:57 +0100

    Asdasdasd

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,15 @@ import './App.css';
 
 
 function App() {
-  navigator.serviceWorker.register('service-worker.js')
 
-  // Client-Side JavaScript (App.tsx or another appropriate file)
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(function (registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-        // Wait until the service worker is activated
-        return navigator.serviceWorker.ready;
-      })
-      .then(function () {
-        showWelcomeNotification();
-      })
-      .catch(function (error) {
-        console.error('Service Worker registration failed:', error);
-      });
-  }
-
-  function showWelcomeNotification() {
-    if (Notification.permission === 'granted') {
-      new Notification('Welcome!', {
-        body: 'Hello, welcome to our website!',
-      });
-    } else if (Notification.permission !== 'denied') {
-      Notification.requestPermission().then(function (permission) {
-        if (permission === 'granted') {
-          new Notification('Welcome!', {
-            body: 'Hello, welcome to our website!',
-          });
-        }
+  navigator.serviceWorker.register('sw.js');
+  Notification.requestPermission(function (result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function (registration) {
+        registration.showNotification('Notification with ServiceWorker');
       });
     }
-  }
-
+  });
   return (
     <>
       <button >
