b2a2e6e2bbbce6522f5b11b341a761bd66955a5d
Lanabi Mohamed <127102977+medlnb@users.noreply.github.com>
2023-11-24 00:03:38 +0100

    adasd

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+// sw.js
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/App.css',
+        // Add other static assets you want to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  // Perform any necessary updates or cleanup tasks
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
